195e71326924748cbd490ff103789b4cbbb1443e
Nidhi <nidhixpatel@gmail.com>
2019-04-09 11:12:24 -0700

    finished

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,12 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), one loop that runs the number of times equal to the input number
+# Space complexity: constant, storing total in one variable. 
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError, "Nil is not a valid input!" if number == nil
+  total = 1
+  until number == 0
+    total = total * number
+    number -= 1
+  end
+  return total
 end
